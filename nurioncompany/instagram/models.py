cfa76b9b0f09e1639e974c3448e26965c7639909
Hae-jin-park <phj30576@hotmail.com>
2023-11-08 11:11:45 +0900

    OneToOneField

--- nurioncompany/instagram/models.py
+++ nurioncompany/instagram/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
@@ -15,3 +18,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-id']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
