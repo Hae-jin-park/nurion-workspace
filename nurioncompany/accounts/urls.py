111b446065f8fd70ff85b8b767633e43d3e82341
Hae-jin-Park <phj30576@hotmail.com>
2023-11-17 13:40:27 +0900

    logout process

--- nurioncompany/accounts/urls.py
+++ nurioncompany/accounts/urls.py
@@ -1,8 +1,13 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
+from .forms import LoginAuthForm
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name="login"),
+    path('login/', LoginView.as_view(
+      form_class=LoginAuthForm,
+      template_name='accounts/login_form.html'), name="login"),
     path('profile/', views.profile, name='profile'),
-    path('profile/update/', views.profile_update, name='profile_update')
+    path('profile/update/', views.profile_update, name='profile_update'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- nurioncompany/accounts/views.py
+++ nurioncompany/accounts/views.py
@@ -1,15 +1,21 @@
+from django.conf import settings
 from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model, login as auth_login
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView, UpdateView, CreateView
 from accounts.models import Profile
 from django.contrib.auth.mixins import LoginRequiredMixin
 from accounts.forms import ProfileForm
 # Create your views here.
 
+User = get_user_model()
 
 # @login_required
 # def profile(request):
 #     return render(request, 'accounts/profile.html')
+
+
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
 
@@ -33,7 +39,7 @@ def profile_update(request):
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             profile = form.save(commit=False)
-            profile.user= request.user
+            profile.user = request.user
             profile.save()
             return redirect('profile')
     else:
@@ -41,3 +47,26 @@ def profile_update(request):
     return render(request, 'accounts/profile_form.html', {
         'form': form,
     })
+
+# signup = CreateView.as_view(
+#     model=User,
+#     form_class=UserCreationForm,
+#     success_url=settings.LOGIN_URL,
+#     template_name = 'accounts/signup_form.html'
+# )
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = 'accounts/signup_form.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        auth_login(self.request, user)
+        return response
+
+
+signup = SignupView.as_view()
